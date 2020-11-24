a0c92993ea999b963205e4bf1b136cc63ccc36bf
Klaus Crusius <klaus.crusius@web.de>
2020-11-24 15:59:37 +0100

    remote tests on travis only with macos nightly builts

--- test/remote.jl
+++ test/remote.jl
@@ -2,15 +2,19 @@
 #download from remote site
 import MatrixDepot: URL_REDIRECT, load, loadinfo, loadsvd
 
-# test only for julia nightly
-if length(VERSION.prerelease) == 0
+# test only for julia nightly on macos or if local
+function do_test_remote()
+    (length(VERSION.prerelease) > 0 && Sys.isapple()) ||
+    !occursin("travis", Sys.BINDIR)
+end
+
+if !do_test_remote()
     println("remote tests not executed")
 else
     URL_REDIRECT[] = 0
     # sp
     pattern = "*/494_bus" # which has not been touched in other tests
     @test loadinfo(pattern) == 1 # load only header
-    println("downloading svd for $pattern")
     @test loadsvd(pattern) == 1 # load svd extension data
     @test load(pattern) == 1 # loaded full data
 
@@ -18,4 +22,5 @@ else
     pattern = "*/*/494_bus" # which has not been touched in other tests
     @test loadinfo(pattern) == 1 # load only header
     @test load(pattern) == 1 # loaded full data
+    @test loadsvd(pattern) == 0 # not available
 end
